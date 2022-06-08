33ef63c7638f37d4fdc953ce02d096e499de8867
Kareem Oluwatobi <toboibo@icloud.com>
2022-06-08 07:52:21 -0700

    Project Setup

---
+++ src/Data/Slim.Data/Context/ApplicationDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace Slim.Data.Context
+{
+    public class SlimDbContext : IdentityDbContext
+    {
+        public SlimDbContext(DbContextOptions<SlimDbContext> options)
+            : base(options)
+        {
+        }
+    }
+}
---
+++ src/Shared/Slim.Shared/Class1.cs
@@ -0,0 +1,7 @@
+﻿namespace Slim.Shared
+{
+    public class Class1
+    {
+
+    }
+}
